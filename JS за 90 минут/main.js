140a3a45e8856576264fde57410eef1a8f87ed5a
rejiserbrazzers <wropdrop@gmail.com>
2019-06-17 17:48:05 +0300

    first commit

---
+++ JS за 90 минут/main.js
@@ -0,0 +1,51 @@
+﻿
+var MyMessege;
+myMessage = "Hellp Nikita you are clever";
+
+
+var myNumber = 10.484;
+var myString = "hFghD";
+var myObject = {
+
+	name: "VASIA",
+	age: 25
+};
+
+
+//console.log( 40 + myNumber);
+//console.log(40 - myNumber);
+//console.log(40* myNumber);
+//console.log(myNumber);
+//myNumber +=10;
+//console.log(myNumber);
+//console.log(Math.round(5.55));
+//console.log(Math.floor(5.9));
+
+//var newNumber = 2.430;
+//console.log(newNumber.toFixed(1));
+
+//console.log("40" + myNumber);
+//console.log(myString.toLowerCase());
+
+
+//МАСИВы
+//console.log(names[0].toUpperCase());
+
+//names.push("Света");
+//console.log(names);
+
+//УСЛОВИЯ 
+
+if ("Вася"!== "Петя" || myNumber > 20 ){
+
+	console.log("TRUE mafaka")
+}
+
+
+if(myNumber < 20 ){
+}
+else
+{
+console.log("Число меньше 20");
+
+}
